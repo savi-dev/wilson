c1cd8004911d404e5b9e4cc7835be20f94e0532b
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2012-02-27 14:57:36 -0500

    Fixing Eliot's commit.
    bug: 11
    Change-Id: I6d445912428a2d91a2cf70efae8129ec666ec95d

--- src/ca/savinetwork/testbed/authentication/Authentication.java
+++ src/ca/savinetwork/testbed/authentication/Authentication.java
@@ -1,27 +1,32 @@
+// Copyright (c) 2012, The SAVI Project.
+// All right researved. http://www.savinetwork.ca
 package ca.savinetwork.testbed.authentication;
 
 /**
  * Authentication class
- * 
- * @author eliot
+ *
+ * @author Eliot Kang <eliot@savinetwork.ca>
  *
  */
+
 
 public interface Authentication {
 
     /**
      * Authenticate resource usage
-     * 
+     *
      * @param AuthenticateResourceUsageIn
-     * @return 
+     * @return
      */
-    public ca.savinetwork.testbed.type.authenticationschema.AuthenticateResourceUsageResponse authenticateResourceUsage(ca.savinetwork.testbed.type.authenticationschema.AuthenticateResourceUsageRequest AuthenticateResourceUsageIn);
+    public ca.savinetwork.testbed.type.authenticationschema.AuthenticateResourceUsageResponse
+        authenticateResourceUsage(ca.savinetwork.testbed.type.authenticationschema.AuthenticateResourceUsageRequest AuthenticateResourceUsageIn);
 
     /**
      * Authenticate user
-     * 
+     *
      * @param AuthenticateUserIn
-     * @return 
+     * @return
      */
-    public ca.savinetwork.testbed.type.authenticationschema.AuthenticateUserResponse authenticateUser(ca.savinetwork.testbed.type.authenticationschema.AuthenticateUserRequest AuthenticateUserIn);
+    public ca.savinetwork.testbed.type.authenticationschema.AuthenticateUserResponse
+        authenticateUser(ca.savinetwork.testbed.type.authenticationschema.AuthenticateUserRequest AuthenticateUserIn);
 }
--- src/ca/savinetwork/testbed/authentication/AuthenticationImpl.java
+++ src/ca/savinetwork/testbed/authentication/AuthenticationImpl.java
@@ -1,15 +1,16 @@
+ // Copyright (c) 2012, The SAVI Project.
+ // All right researved. http://www.savinetwork.ca
+
 package ca.savinetwork.testbed.authentication;
 
 import ca.savinetwork.testbed.type.authenticationschema.*;
 import ca.savinetwork.testbed.type.datastoreschema.*;
-
 import ca.savinetwork.testbed.datastore.DataStore;
 
 /**
  * Authentication class
- * 
- * @author eliot
  *
+ * @author Eliot Kang <eliot@savinetwork.ca>
  */
 public class AuthenticationImpl implements Authentication {
     /**
@@ -22,52 +23,52 @@ public class AuthenticationImpl implements Authentication {
     }
     @Override
     public AuthenticateResourceUsageResponse authenticateResourceUsage(AuthenticateResourceUsageRequest AuthenticateResourceUsageIn) {
-    	AuthenticateResourceUsageResponse AuthenticateResourceUsageOut = new AuthenticateResourceUsageResponse();
-    	AuthenticateResourceUsageResponse AuthenticateResourceUsageOut_Fail = new AuthenticateResourceUsageResponse();
-    	DBGetResourceUsageResponse DBGetResourceUsageOut = new DBGetResourceUsageResponse();
-    	DBGetResourceUsageRequest DBGetResourceUsageIn = new DBGetResourceUsageRequest();
-    	
-    	DBGetResourceUsageIn.setPlanName(AuthenticateResourceUsageIn.getPlanName());
-    	DBGetResourceUsageIn.setAorName(AuthenticateResourceUsageIn.getAorName());
-    	DBGetResourceUsageIn.setResourceType(AuthenticateResourceUsageIn.getResourceType());
-    	DBGetResourceUsageIn.setUuid(AuthenticateResourceUsageIn.getUuid());
-    	DBGetResourceUsageIn.setStartTime(AuthenticateResourceUsageIn.getStartTime());
-    	DBGetResourceUsageIn.setStopTime(AuthenticateResourceUsageIn.getStopTime());
-    	DBGetResourceUsageIn.setInUse(AuthenticateResourceUsageIn.isInUse());
-    	DBGetResourceUsageIn.setUsername(AuthenticateResourceUsageIn.getUsername());
-    	
-    	DBGetResourceUsageOut = dataStore.dbGetResourceUsage(DBGetResourceUsageIn);
-    	
-    	if (DBGetResourceUsageOut.getResourceList() != null 
-    			&& DBGetResourceUsageOut.getResourceList().size() > 0) {
-    		AuthenticateResourceUsageOut.setSuccessful(true);
-    		return AuthenticateResourceUsageOut;
-    	} else {
-    		AuthenticateResourceUsageOut_Fail.setSuccessful(false);
-    		AuthenticateResourceUsageOut_Fail.setError("No such resource");
-    		return AuthenticateResourceUsageOut_Fail;
-    	}
+      AuthenticateResourceUsageResponse AuthenticateResourceUsageOut = new AuthenticateResourceUsageResponse();
+      AuthenticateResourceUsageResponse AuthenticateResourceUsageOut_Fail = new AuthenticateResourceUsageResponse();
+      DBGetResourceUsageResponse DBGetResourceUsageOut = new DBGetResourceUsageResponse();
+      DBGetResourceUsageRequest DBGetResourceUsageIn = new DBGetResourceUsageRequest();
+
+      DBGetResourceUsageIn.setPlanName(AuthenticateResourceUsageIn.getPlanName());
+      DBGetResourceUsageIn.setAorName(AuthenticateResourceUsageIn.getAorName());
+      DBGetResourceUsageIn.setResourceType(AuthenticateResourceUsageIn.getResourceType());
+      DBGetResourceUsageIn.setUuid(AuthenticateResourceUsageIn.getUuid());
+      DBGetResourceUsageIn.setStartTime(AuthenticateResourceUsageIn.getStartTime());
+      DBGetResourceUsageIn.setStopTime(AuthenticateResourceUsageIn.getStopTime());
+      DBGetResourceUsageIn.setInUse(AuthenticateResourceUsageIn.isInUse());
+      DBGetResourceUsageIn.setUsername(AuthenticateResourceUsageIn.getUsername());
+
+      DBGetResourceUsageOut = dataStore.dbGetResourceUsage(DBGetResourceUsageIn);
+
+      if (DBGetResourceUsageOut.getResourceList() != null
+          && DBGetResourceUsageOut.getResourceList().size() > 0) {
+        AuthenticateResourceUsageOut.setSuccessful(true);
+        return AuthenticateResourceUsageOut;
+      } else {
+        AuthenticateResourceUsageOut_Fail.setSuccessful(false);
+        AuthenticateResourceUsageOut_Fail.setError("No such resource");
+        return AuthenticateResourceUsageOut_Fail;
+      }
     }
 
     @Override
     public AuthenticateUserResponse authenticateUser(AuthenticateUserRequest AuthenticateUserIn) {
-    	DBGetUserResponse DBGetUserOut = new DBGetUserResponse();
-    	DBGetUserRequest DBGetUserIn = new DBGetUserRequest();
-    	AuthenticateUserResponse AuthenticateUserOut = new AuthenticateUserResponse();
-    	AuthenticateUserResponse AuthenticateUserOut_AuthFail = new AuthenticateUserResponse();
-    
-    	DBGetUserIn.setUserCredentials(AuthenticateUserIn.getUserCredentials());
-    	
-    	//	<invoke>
-    	DBGetUserOut = dataStore.dbGetUser(DBGetUserIn);
-    	
-    	if (DBGetUserOut.getUsers() != null && DBGetUserOut.getUsers().size() > 0) {
-    		AuthenticateUserOut.setSuccessful(true);
-    		return AuthenticateUserOut;
-    	} else {
-    		AuthenticateUserOut_AuthFail.setSuccessful(false);
-    		AuthenticateUserOut_AuthFail.setError("Invalid username/password");
-    		return AuthenticateUserOut_AuthFail;
-    	}
+      DBGetUserResponse DBGetUserOut = new DBGetUserResponse();
+      DBGetUserRequest DBGetUserIn = new DBGetUserRequest();
+      AuthenticateUserResponse AuthenticateUserOut = new AuthenticateUserResponse();
+      AuthenticateUserResponse AuthenticateUserOut_AuthFail = new AuthenticateUserResponse();
+
+      DBGetUserIn.setUserCredentials(AuthenticateUserIn.getUserCredentials());
+
+      //  <invoke>
+      DBGetUserOut = dataStore.dbGetUser(DBGetUserIn);
+
+      if (DBGetUserOut.getUsers() != null && DBGetUserOut.getUsers().size() > 0) {
+        AuthenticateUserOut.setSuccessful(true);
+        return AuthenticateUserOut;
+      } else {
+        AuthenticateUserOut_AuthFail.setSuccessful(false);
+        AuthenticateUserOut_AuthFail.setError("Invalid username/password");
+        return AuthenticateUserOut_AuthFail;
+      }
     }
 }
